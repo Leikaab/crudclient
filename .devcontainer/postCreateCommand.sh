6732d5f211b9bc18fc86f271e71292224f38fe70
leikaab <nordavindltd@gmail.com>
2024-08-13 07:29:12 +0200

    chore: postCreateCommand for possiblity of local git user and pre-commit hooks in devcontainer. For now its commented out, since we are using git from host computer and we havent set up pre-commit yet

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# setup local git user for the container, commented out for now
+#git config --global user.email "nordavindltd@gmail.com"
+#git config --global user.name "leikaab"
+
+# set up pre-commit hooks, commented out for now
+#poetry run pre-commit install -t pre-commit
+#poetry run pre-commit install -t pre-push
+
